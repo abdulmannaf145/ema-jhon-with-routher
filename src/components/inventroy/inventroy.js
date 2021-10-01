7802a4bd65011db2c04a54481d962e61a4826f4a
abdulmannaf145 <mdabdulmannaf145@gmail.com>
2021-10-01 11:36:46 +0600

    inventory added

---
+++ src/components/inventroy/inventroy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const inventroy = () => {
+    return (
+        <div>
+            <h2>This is Inventroy</h2>
+        </div>
+    );
+};
+
+export default inventroy;
